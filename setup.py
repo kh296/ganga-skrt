c922429865e27d0066e01acfce6bc0ead9d32023
K. Harrison <kh296@cam.ac.uk>
2021-11-13 18:36:22 +0000

    Updated environment and setup

--- setup.py
+++ setup.py
@@ -10,16 +10,15 @@ setup(
     license='',
     packages=find_packages(),
     install_requires=[
-                      'ganga==8.5.0',
+                      'ganga==8.5.5',
                       'in_place',
                      ],
-    scripts=['examples/bin/create_config'],
+    scripts=['examples/bin/create_config', 'examples/bin/create_setup'],
     classifiers=[
         'Development Status :: 1 - Planning',
         'Intended Audience :: Science/Research',
         'License :: To be decided :: To be decided',  
         'Operating System :: POSIX :: Linux',        
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.8',
     ],
 )
